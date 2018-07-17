ea2ad6de1abe85765bf796987f4879a0c54ba0d4
Obilarius <git@walzenbach.com>
2018-07-17 16:35:41 +0200

    Match Complete Name from XML to Title, MiddleName and Suffix

--- Redemption_v2.0.1.ps1
+++ Redemption_v2.0.1.ps1
@@ -15,40 +15,82 @@ $Contacts = [Microsoft.Exchange.WebServices.Data.Folder]::Bind($service,$folderi
 
 
 ### XML
-
 [xml]$xml = get-content "D:\ARGESContactList.xml"
 
 $xml_contacts = $xml.Report.Tablix1.Details_Collection
 $xml_contactArr = New-Object System.Collections.Generic.List[System.Object]
 
+
+
+
+
 foreach ($ContactObj in $xml_contacts.ChildNodes)
 {
-    #if($ContactObj.GivenName -eq "Vorname") {
-        $Contact = New-Object Microsoft.Exchange.WebServices.Data.Contact($service)
+    $Contact = New-Object Microsoft.Exchange.WebServices.Data.Contact($service)
 
-        $Contact.GivenName = $ContactObj.GivenName
-        $Contact.Surname = $ContactObj.SurName
-        $Contact.FileAs = $ContactObj.CompleteName
-        #if($Title -ne ""){
-	    #    $PR_DISPLAY_NAME_PREFIX_W = New-Object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition(0x3A45,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::String);  
-	    #    $Contact.SetExtendedProperty($PR_DISPLAY_NAME_PREFIX_W,$Title)						
-        #}
-        if($ContactObj.CompanyName) { $Contact.CompanyName = $ContactObj.CompanyName }
-        if($ContactObj.BusinssPhone) { $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::BusinessPhone] = $ContactObj.BusinssPhone }
-        if($ContactObj.MobilePhone) { $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::MobilePhone] = $ContactObj.MobilePhone }
-        if($ContactObj.HomePhone) { $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::HomePhone] = $ContactObj.HomePhone }
-                    
-        $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business] = New-Object  Microsoft.Exchange.WebServices.Data.PhysicalAddressEntry
-        if($ContactObj.BusinessStreet) { $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].Street = $ContactObj.BusinessStreet }
-        if($ContactObj.BusinessState) { $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].State = $ContactObj.BusinessState }
-        if($ContactObj.BusinessCity) { $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].City = $ContactObj.BusinessCity }
-        if($ContactObj.BusinessCountryOrRegion) { $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].CountryOrRegion = $ContactObj.BusinessCountryOrRegion }
-        if($ContactObj.BusinessPostalCode) { $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].PostalCode = $ContactObj.BusinessPostalCode }
+    
+    ### Scan Complete Name from XML to Title, MiddleName and Suffix
+    $regex = "^(.*) " + $ContactObj.GivenName
+    $Title = $ContactObj.CompleteName -Split $regex
+    $Title = $CompleteName[1]
+        
+    $regex = $ContactObj.GivenName + " (.*) " + $ContactObj.SurName
+    $MiddleName = $ContactObj.CompleteName -Split $regex
+    $MiddleName = $MiddleName[1]
 
-        $Contact.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1] = $ContactObj.Email1
-        if($ContactObj.Notes) { $Contact.Body = $ContactObj.Notes }
+    $regex = $ContactObj.SurName + " (.*)"
+    $Suffix = $ContactObj.CompleteName -Split $regex
+    $Suffix = $Suffix[1]
+        
 
-        $Contact.Save($Contacts.Id)				
-		Write-Host ($ContactObj.CompleteName + " Contact Created")
-    #}
+    $Contact.GivenName = $ContactObj.GivenName
+    $Contact.Surname = $ContactObj.SurName
+    $Contact.NickName = $ContactObj.NickName
+    $Contact.FileAs = $ContactObj.CompleteName
+    if($Title -ne ""){
+	    $PR_DISPLAY_NAME_PREFIX_W = New-Object Microsoft.Exchange.WebServices.Data.ExtendedPropertyDefinition(0x3A45,[Microsoft.Exchange.WebServices.Data.MapiPropertyType]::String);  
+	    $Contact.SetExtendedProperty($PR_DISPLAY_NAME_PREFIX_W,$Title)						
+    }
+    $Contact.MiddleName = $MiddleName
+    $Contact.Generation = $Suffix
+
+    $Contact.CompanyName = $ContactObj.CompanyName
+    $Contact.JobTitle = $ContactObj.JobTitle
+
+    $Contact.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress1] = $ContactObj.Email1
+    $Contact.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress2] = $ContactObj.Email2
+    $Contact.EmailAddresses[[Microsoft.Exchange.WebServices.Data.EmailAddressKey]::EmailAddress3] = $ContactObj.Email3
+    $Contact.BusinessHomePage = $ContactObj.BusinessHomePage
+
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::AssistantPhone] = $ContactObj.AssistantPhone
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::BusinessFax] = $ContactObj.BusinessFax
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::BusinessPhone] = $ContactObj.BusinessPhone
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::BusinessPhone2] = $ContactObj.BusinessPhone2
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::CompanyMainPhone] = $ContactObj.CompanyMainPhone
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::HomePhone] = $ContactObj.HomePhone
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::HomePhone2] = $ContactObj.HomePhone2
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::MobilePhone] = $ContactObj.MobilePhone
+    $Contact.PhoneNumbers[[Microsoft.Exchange.WebServices.Data.PhoneNumberKey]::PrimaryPhone] = $ContactObj.PrimaryPhone
+
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].Street = $ContactObj.BusinessStreet
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].City = $ContactObj.BusinessCity
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].CountryOrRegion = $ContactObj.BusinessCountryOrRegion
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].PostalCode = $ContactObj.BusinessPostalCode
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Business].State = $ContactObj.BusinessState
+ 
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Home].Street = $ContactObj.HomeStreet
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Home].City = $ContactObj.HomeCity
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Home].CountryOrRegion = $ContactObj.HomeCountryOrRegion
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Home].PostalCode = $ContactObj.HomePostalCode
+    $Contact.PhysicalAddresses[[Microsoft.Exchange.WebServices.Data.PhysicalAddressKey]::Home].State = $ContactObj.HomeState
+
+    $Contact.Department = $ContactObj.Department
+    $Contact.Profession = $ContactObj.Profession
+    $Contact.Birthday = $ContactObj.Birthday
+    $Contact.WeddingAnniversary = $ContactObj.WeddingAnniversary
+
+    $Contact.Body = $ContactObj.Note
+    
+    $Contact.Save($Contacts.Id)				
+	Write-Host ($ContactObj.CompleteName + " Contact Created")
 }
